a8e7f4c00091cf2e210c98d5d44835890e6579ff
James Turk <james.p.turk@gmail.com>
2013-07-25 13:13:51 -0400

    use settings

--- imago/core.py
+++ imago/core.py
@@ -1,4 +1,8 @@
 import pymongo
+from django.conf import settings
 
-db = pymongo.Connection()['opencivicdata']
+# http://docs.mongodb.org/manual/reference/connection-string/
+mongo_uri = getattr(settings, 'IMAGO_MONGO_URI', 'localhost')
+mongo_db = getattr(settings, 'IMAGO_MONGO_DATABASE', 'opencivicdata')
 
+db = pymongo.Connection(mongo_uri)[mongo_db]
