9a43b3fc8f3d93ef9651ecc5853daa0fea52e430
James McKinney <james@slashpoundbang.com>
2014-02-12 17:37:08 -0500

    pyelasticsearch is a requirement

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(name='imago',
       long_description='',
       platforms=['any'],
       install_requires=[
+          'pyelasticsearch>=0.6',
           'pymongo>=2.5',
           'Django>=1.5',
       ])
