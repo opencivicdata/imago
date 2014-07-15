1a00f8d36f89b0eb0cecd94708ff0e50bcfca81e
Paul Tagliamonte <tag@pault.ag>
2014-07-15 14:13:41 -0400

    Start to bulk out default org views

--- imago/views.py
+++ imago/views.py
@@ -10,17 +10,54 @@ JURISDICTION_SERIALIZE = defaultdict(dict, [
     ("feature_flags", lambda x: x.feature_flags),
     ("division", DIVISION_SERIALIZE),
 ])
-ORGANIZATION_SERIALIZE = defaultdict(dict)
-PERSON_SERIALIZE = defaultdict(dict)
-BILL_SERIALIZE = defaultdict(dict)
-VOTE_SERIALIZE = defaultdict(dict)
-EVENT_SERIALIZE = defaultdict(dict)
+
+ORGANIZATION_SERIALIZE = defaultdict(dict, [
+    ("jurisdiction", JURISDICTION_SERIALIZE),
+])
+ORGANIZATION_SERIALIZE['parent'] = ORGANIZATION_SERIALIZE
+
+PERSON_SERIALIZE = defaultdict(dict, [
+])
+
+POST_SERIALIZE = defaultdict(dict, [
+    ("organization", ORGANIZATION_SERIALIZE),
+    ("division", DIVISION_SERIALIZE),
+])
+
+ORGANIZATION_SERIALIZE['posts'] = POST_SERIALIZE
+
+MEMBERSHIP_SERIALIZE = {
+    # Explicit to avoid letting `id' leak out.
+    "start_date": {},
+    "end_date": {},
+    "role": {},
+    "label": {},
+    "organization": ORGANIZATION_SERIALIZE,
+    "person": PERSON_SERIALIZE,
+    "post": POST_SERIALIZE,
+    "on_behalf_of": ORGANIZATION_SERIALIZE,
+}
+
+ORGANIZATION_SERIALIZE['memberships'] = MEMBERSHIP_SERIALIZE
+PERSON_SERIALIZE['memberships'] = MEMBERSHIP_SERIALIZE
+POST_SERIALIZE['memberships'] = MEMBERSHIP_SERIALIZE
+
+
+BILL_SERIALIZE = defaultdict(dict, [
+])
+
+VOTE_SERIALIZE = defaultdict(dict, [
+])
+
+EVENT_SERIALIZE = defaultdict(dict, [
+])
 
 
 class JurisdictionList(PublicListEndpoint):
     model = Jurisdiction
     serialize_config = JURISDICTION_SERIALIZE
-    default_fields = ['name', 'extras']
+    default_fields = ['id', 'name', 'url', 'classification', 'feature_flags',
+                      'division.id', 'division.display_name']
 
     def adjust_filters(self, params):
         if 'name' in params:
@@ -30,14 +67,20 @@ class JurisdictionList(PublicListEndpoint):
 
 class JurisdictionDetail(PublicDetailEndpoint):
     model = Jurisdiction
-    serialize_config = JurisdictionList.serialize_config
-    default_fields = []
+    serialize_config = JURISDICTION_SERIALIZE
+    default_fields = JurisdictionList.default_fields
 
 
 class OrganizationList(PublicListEndpoint):
     model = Organization
-    serialize_config = {}
-    default_fields = serialize_config.keys()
+    serialize_config = ORGANIZATION_SERIALIZE
+    default_fields = ['id', 'name', 'image', 'classification',
+                      'jurisdiction.id',
+                      'parent.id', 'parent.name',
+                      'memberships.person.id', 'memberships.person.name',
+                      'memberships.post.id',
+                      'memberships.post.label', 'memberships.post.role',
+                     ]
 
 
 class OrganizationDetail(PublicDetailEndpoint):
