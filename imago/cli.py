08d6c2cce8c0730201c1d40583713438e267a293
Paul Tagliamonte <tag@pault.ag>
2014-07-30 10:44:09 -0400

    Update view.

--- imago/cli.py
+++ imago/cli.py
@@ -12,8 +12,18 @@ def debug():
         print("")
 
     connection = debug['connection']
+    queries = connection['query']['list']
 
     print("Made %s queries" % (connection['query']['count']))
+    print("Sorted by time:")
+    print("")
+    for query in sorted(queries, reverse=True, key=lambda x: float(x['time'])):
+        sql = query['sql']
+        if len(sql) >= 80:
+            sql = sql[:80] + "..."
+
+        print("  %s:   %s" % (query['time'], sql))
+
     print("")
     print("Prefetched Fields:")
     for field in debug['prefetch_fields']:
