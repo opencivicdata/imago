cf17da42dc101c3f8baba8a2ad5e046c978fd74d
James Turk <james.p.turk@gmail.com>
2014-08-07 11:07:00 -0400

    remove stale exceptions file

--- imago/exceptions.py
+++
@@ -1,9 +0,0 @@
-
-class APIError(Exception):
-
-    def __init__(self, msg, status=400):
-        self.msg = msg
-        self.status = status
-
-    def __str__(self):
-        return str(self.msg)
