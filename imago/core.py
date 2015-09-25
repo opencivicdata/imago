b275f96f92942c1d4b1062cb181499ab604db0fa
James Turk <james.p.turk@gmail.com>
2015-09-25 12:15:45 -0400

    drop ES references

--- imago/core.py
+++
@@ -1,7 +0,0 @@
-import pyelasticsearch
-from django.conf import settings
-
-if getattr(settings, 'ENABLE_ELASTICSEARCH', True):
-    host = getattr(settings, 'ELASTICSEARCH_HOST', 'localhost')
-    timeout = getattr(settings, 'ELASTICSEARCH_TIMEOUT', 60)
-    elasticsearch = pyelasticsearch.ElasticSearch(host, timeout=timeout, revival_delay=0)
